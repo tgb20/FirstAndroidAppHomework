f94d6e4b6f3ac6159ea85ac8f66f382ec72f53cf
Jacob Bashista <jacob@orange.haus>
2017-10-16 17:29:02 -0400

    Initial commit

---
+++ app/src/main/java/haus/orange/firstandroidapphomework/ImageActivity.kt
@@ -0,0 +1,12 @@
+package haus.orange.firstandroidapphomework
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ImageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image)
+    }
+}
